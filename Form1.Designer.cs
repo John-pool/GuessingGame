89bd398bf11090da8e74d8611f572359f5919b9c
Saberon, John Paul (JeyP) <johnpaulsaberon1428@gmail.com>
2024-09-07 20:42:33 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace PrelimsTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_mask_word = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.answer = new System.Windows.Forms.TextBox();
+            this.guess = new System.Windows.Forms.Button();
+            this.list = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lb_mask_word
+            // 
+            this.lb_mask_word.AutoSize = true;
+            this.lb_mask_word.Font = new System.Drawing.Font("Microsoft YaHei", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_mask_word.Location = new System.Drawing.Point(23, 43);
+            this.lb_mask_word.Name = "lb_mask_word";
+            this.lb_mask_word.Size = new System.Drawing.Size(288, 39);
+            this.lb_mask_word.TabIndex = 0;
+            this.lb_mask_word.Text = "GUESS THE WORD";
+            this.lb_mask_word.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Unicode MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(347, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Wrong guessed";
+            // 
+            // answer
+            // 
+            this.answer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.answer.BackColor = System.Drawing.Color.White;
+            this.answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answer.Location = new System.Drawing.Point(51, 90);
+            this.answer.Name = "answer";
+            this.answer.Size = new System.Drawing.Size(223, 29);
+            this.answer.TabIndex = 2;
+            // 
+            // guess
+            // 
+            this.guess.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.guess.BackColor = System.Drawing.Color.White;
+            this.guess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guess.ForeColor = System.Drawing.Color.Black;
+            this.guess.Location = new System.Drawing.Point(51, 168);
+            this.guess.Name = "guess";
+            this.guess.Size = new System.Drawing.Size(218, 80);
+            this.guess.TabIndex = 4;
+            this.guess.Text = "guess";
+            this.guess.UseVisualStyleBackColor = false;
+            this.guess.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // list
+            // 
+            this.list.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.list.BackColor = System.Drawing.Color.White;
+            this.list.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.list.FormattingEnabled = true;
+            this.list.Location = new System.Drawing.Point(322, 90);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(227, 147);
+            this.list.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(591, 334);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.guess);
+            this.Controls.Add(this.answer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lb_mask_word);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_mask_word;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox answer;
+        private System.Windows.Forms.Button guess;
+        private System.Windows.Forms.ListBox list;
+    }
+}
+
